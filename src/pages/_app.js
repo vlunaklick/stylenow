67ed28d76e1a2bee6593badf89778b324271d45a
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-02-21 21:03:04 -0300

    add tailwind and clean project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
+import { Inter } from '@next/font/google'
+
 import '@/styles/globals.css'
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        body {
+          font-family: ${inter.fontFamily}, system-ui, sans-serif;
+        }
+      `}</style>
+    </>
+  )
 }
