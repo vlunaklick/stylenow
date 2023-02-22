a747dd4e30d77abc3173f66fab760f614828bb60
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-02-22 17:13:58 -0300

    add ClickToTry component to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 
-import Header from '@/components/Header'
 import Hero from '@/components/Hero'
-import TestImages from '@/components/TestImages'
+import ClickToTry from '@/components/sections/ClickToTry'
 
 export default function Home() {
   return (
@@ -11,11 +10,9 @@ export default function Home() {
         <title>Cropnow</title>
       </Head>
 
-      <Header />
-
       <main className="max-w-3xl mx-auto mt-4 w-full">
         <Hero />
-        <TestImages />
+        <ClickToTry />
       </main>
     </>
   )
