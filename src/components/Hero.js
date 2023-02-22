a37658b1d7c5943e19a406b9dc8c21713fce549d
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-02-21 23:17:06 -0300

    add Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,30 @@
+import { useImage } from '@/context/Image'
+import UploadFile from './UploadFile'
+
+export default function Hero() {
+  const { imageURL } = useImage()
+
+  return (
+    <>
+      <div className="flex gap-6 py-16 px-4 sm:py-14 flex-col md:flex-row">
+        <section>
+          <h2 className="text-3xl font-extrabold tracking-tight text-black sm:text-4xl lg:text-6xl">
+            <span className="block text-indigo-400">
+              Crop your photo instantly!
+            </span>
+          </h2>
+          <p className="mt-5 max-w-3xl text-xl text-gray-600">
+            CropNow is a free online tool for easy image cropping and resizing.
+            Get perfect dimensions and aspect ratios for your website, social
+            media, and more. Say goodbye to manual editing and try CropNow
+            today!
+          </p>
+        </section>
+
+        <section className="h-full">
+          <UploadFile />
+        </section>
+      </div>
+    </>
+  )
+}
