238c63d4ef107f5c8903f1fea94df4c5164cdcdd
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-02-21 23:17:54 -0300

    add standard structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
+import Head from 'next/head'
+
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+
 export default function Home() {
   return (
     <>
-      <h1>Cropnow</h1>
+      <Head>
+        <title>Cropnow</title>
+      </Head>
+
+      <Header />
+
+      <main className="max-w-3xl mx-auto mt-4 w-full">
+        <Hero />
+      </main>
     </>
   )
 }
