be800b779c0e417111fbf004ee78f71b9f8d0d17
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-03-04 00:44:34 -0300

    add variants to button

--- src/components/EditorButton.js
+++ src/components/EditorButton.js
@@ -1,7 +1,20 @@
-export default function EditorButton({ children, onClick }) {
+const VARIANT = {
+  primary:
+    'border-slate-400 text-slate-600 hover:text-slate-700 hover:border-slate-500',
+  secondary:
+    'border-slate-200 text-slate-500 hover:text-slate-600 hover:border-slate-300',
+}
+
+export default function EditorButton({
+  children,
+  onClick,
+  variant = 'primary',
+}) {
   return (
     <button
-      className="rounded flex-1 border-slate-300 border-2 p-1 text-slate-600 hover:text-slate-700 hover:border-slate-400 transition-colors"
+      className={
+        'rounded flex-1 border-2 p-1 transition-colors' + ' ' + VARIANT[variant]
+      }
       onClick={onClick}
     >
       {children}
